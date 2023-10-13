0a6529b64409973314ad39a0010c0affbfdae726
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2023-10-13 10:20:18 +0600

    Create c++ v4.cpp

---
+++ c++ v4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+
+cout<<"Md."<<"Shahriar"<<"Chowdhury"<<endl;
+cout<<"Md. Shahriar Chowdhury"<<endl;
+cout<<"Md."<<endl<<"Shahriar"<<endl<<"Chowdhury"<<endl;
+
+getch();
+/* return 0;      If i dont want to see the other written  bellow the console
+                  then i have to write getch () and i also have to write a
+                  header file include(conio.h)*/
+}
