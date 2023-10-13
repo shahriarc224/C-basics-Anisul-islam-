ed9638fc77fa9815f3dc22daa2debb862313e404
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2023-10-13 20:18:22 +0600

    Create c++v 8.cpp

---
+++ c++v 8.cpp
@@ -0,0 +1,16 @@
+//How to show the out put.
+
+#include<iostream>
+using namespace std;
+int main ()
+{
+
+    int num1=10,num2=20;
+     char ch='a';
+    cout<<"Numer 1:"<<num1<<endl<<"Number 2:"<<num2<<endl;
+
+    cout<<"Character:"<<ch<<endl;
+
+
+    return 0;
+}
