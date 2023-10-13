f1df4942737a835c28ce598e10ed31bd7d5d70a6
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2023-10-13 22:13:28 +0600

    Create c++ v10.cpp

---
+++ c++ v10.cpp
@@ -0,0 +1,27 @@
+//How to take input and declare it .......
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int num1;
+    char ch;
+    float num2;
+    double num3;
+
+    cout<<"Enter an integer number:"<<endl;
+    cout<<"Enter a character:"<<endl;
+    cout<<"Enter a float number:" <<endl;
+    cout<<"Enter an double nuymber:"<<endl;
+
+    cin>>num1>>ch>>num2>>num3;
+
+    cout<<"Entered integer number:"<<num1<<endl;
+    cout<<"Entered character:"<<ch<<endl;
+    cout<<"Entered float number: "<<num2<<endl;
+    cout<<"Entered double nuymber:"<<num3<<endl;
+
+
+    getch ();
+}
