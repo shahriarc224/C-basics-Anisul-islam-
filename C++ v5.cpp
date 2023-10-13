25f690b0db10ebce6253b11b97590581d216ed86
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2023-10-13 10:48:17 +0600

    Create C++ v5.cpp

---
+++ C++ v5.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+ int main(){
+ cout<<"Md.\n Shahriar \n Chowdhuhry\n"; //we can also us e this /n instade of endl.
+
+  cout<<"\"Md.Shahriar Chowdhury\"\n";
+                                            /*If i want to use double cotetion   the we
+                                           have to use  a back slash sigh  infront of
+                                            the double cotetion .*/
+
+  cout<<"\tShahriar";       //  \t is going  to give space
+
+  
+  cout<<"Shahriar"<<endl;      //we can use endl instade of \n. 
+  
+  
+ getch ();
+ }
