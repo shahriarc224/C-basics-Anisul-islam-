a66bc730b99353afe6a63d8d45e122786e5ceca3
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2023-10-13 11:45:20 +0600

    Create C++ v7 .cpp

---
+++ C++ v7 .cpp
@@ -0,0 +1,50 @@
+/*how to declare  datattype, initialige it and show its out put */
+
+
+#include<iostream>
+using namespace std ;
+int main(){
+
+int num1,num2;
+num1=10;
+num2=30;
+
+int sum=num1+num2;
+
+cout<<"Sum:"<<sum<<endl;
+                /*Thats how we declare a datatype and variable  */
+
+return 0;
+}
+
+
+/*How to eclare integer,flote ,char,double,string datatypes and initialige it  */
+
+
+#include<iostream>
+using namespace std ;
+int main()
+{
+
+    int num1=10;
+    float num2=2.5;
+    double num3=23.698;
+    char num4='A';
+    string num5="Shahriar";
+
+    cout<<"Num1="<<num1<<endl<<"Num2="<<num2<<endl<<"Num2="<<num2<<endl<<"Num3="<<num3<<endl<<"Num4="<<num4<<endl<<"Num5="<<num5<<endl;
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
