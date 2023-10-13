de2abb42ddcfcafc793de596c637567b39e8af71
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2023-10-13 20:29:19 +0600

    Create c++v9.cpp

---
+++ c++v9.cpp
@@ -0,0 +1,14 @@
+//Howo to show out put of string
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+
+string name="Md Shahriar Chowdhury";
+
+cout<<"Name:"<<name<<endl;
+
+getch();
+}
