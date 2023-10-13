d022ba825b651b0ef7640b98dc71164f38dec8fb
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2023-10-13 10:26:16 +0600

    Update c++ v4.cpp

--- c++ v4.cpp
+++ c++ v4.cpp
@@ -5,10 +5,10 @@ int main(){
 
 cout<<"Md."<<"Shahriar"<<"Chowdhury"<<endl;
 cout<<"Md. Shahriar Chowdhury"<<endl;
-cout<<"Md."<<endl<<"Shahriar"<<endl<<"Chowdhury"<<endl;
-
+cout<<"Md."<<endl<<"Shahriar"<<endl<<"Chowdhury"<<endl;      //  we can also use \n to write the code bollow one another 
+cout<<"Md.\n Shahriar \n Chowdhuhry";
 getch();
-/* return 0;      If i dont want to see the other written  bellow the console
+// return 0;   /*   If i dont want to see the other written  bellow the console
                   then i have to write getch () and i also have to write a
                   header file include(conio.h)*/
 }
