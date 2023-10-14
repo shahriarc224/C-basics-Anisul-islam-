270e4f5845803f3641a26c28084b49aeac08f736
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2023-10-14 11:15:07 +0600

    Create C++ v11.cpp

---
+++ C++ v11.cpp
@@ -0,0 +1,29 @@
+/*Howo to use arethmetic operators ......
+the use of +,-,*,/,%........
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+int num1,num2;
+cout<<"Enter two numbers:"<<endl;
+cin>>num1>>num2;
+//num1=10,num2=20;          //We can initialige the vatriable
+
+int sum=num1+num2;
+cout<<"Sum is:"<<sum<<endl;
+
+int sub=num1-num2;
+cout<<"Substraction is :"<<sub<<endl;
+
+int mul=num1*num2;
+cout<<"Multiplaction is :"<<mul<<endl;
+
+double div=(float)num1/num2;      //type cdasting
+cout<<"Division is :"<<div<<endl;
+
+int rem=num1%num2;
+cout<<"Reminder is :"<<rem<<endl;
+
+return 0;
+}
