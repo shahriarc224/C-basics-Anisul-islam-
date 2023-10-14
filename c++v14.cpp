b1f4806342a5d211c37721d0e42f45662c982d7b
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2023-10-14 21:58:36 +0600

    Create c++v14.cpp

---
+++ c++v14.cpp
@@ -0,0 +1,14 @@
+//Temperature convertor
+
+#include<iostream>
+uaing namespace std;
+int main(){
+
+double faren,cels;
+
+cout<<"Enter celcus:"<<endl;
+   
+farn=1.8*cels+32;   //formula for celcus
+
+    return 0;
+}
