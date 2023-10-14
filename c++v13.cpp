794ca4f50b20049d4442252ff61cd4cd762e1d7c
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2023-10-14 21:47:24 +0600

    Create c++v13.cpp

---
+++ c++v13.cpp
@@ -0,0 +1,21 @@
+//How to calculate area of a triangle,circle,rectangle...
+
+#include<iostream>
+using namespace std;
+int main (){
+
+double base,hight;
+
+cout<<"Enter base:"<<endl;
+cin>>base;
+
+cout<<"Enter hight:"<<endl;
+cin>>hight ;
+
+double area =0.5*base*hight;     //Formula of triangle..
+
+cout<<"Area of triangle="<<area<<endl;
+
+return 0;
+
+}
