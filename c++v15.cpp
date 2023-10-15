0ae9b9f0c70c948c65d6bb65e20ffa0bec8f019f
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2023-10-15 19:32:42 +0600

    Create c++v15.cpp

---
+++ c++v15.cpp
@@ -0,0 +1,16 @@
+/*How to use assignment operator =,+=,-= ,*=, /= ,%= ..*/
+
+#include<iostream>
+using namespace std;
+int main(){
+
+ int x=3,int y=2;
+
+ x+=y;  //x=x+y
+ cout<<x<<endl;
+
+ x-=;   //x=x-y
+cout<<x<<endl;
+
+    retrun 0;
+}
