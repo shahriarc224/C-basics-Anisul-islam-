e0968db09ea3c6cf1edd31ab4f62444f017b3bf2
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2023-10-15 21:53:44 +0600

    Create c++v18(part2).cpp

---
+++ c++v18(part2).cpp
@@ -0,0 +1,17 @@
+/*How to   */
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+
+    int a=32,b=12,c;
+
+    c=a>>3;  //we are shifting 3 unit or we are doing 32/2 three times
+    c=<<3;    //we are doing 32*2 three times 
+    cout<<c<<end;
+
+    getch();
+
+}
