4c08f33ae33388ce22221b9737f8bbbf45f5c539
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2023-10-15 21:52:14 +0600

    Create c++v17([part1).cpp

---
+++ c++v17([part1).cpp
@@ -0,0 +1,22 @@
+/*How to use bit wise &,bitwise or |,bitwise xor ^  */
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+
+    int a=32,b=12,c;
+
+    c=a&b;
+    cout<<c<<endl;
+
+    c=a|b;
+    cout<<c<<endl;
+
+    c=a^b;
+    cout<<c<<endl;
+
+
+  getch();
+}
