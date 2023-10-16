6ce40fad7eb5637807a4194d226b5f45135cc5fb
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2023-10-16 20:16:42 +0600

    Create c++v24.cpp

---
+++ c++v24.cpp
@@ -0,0 +1,24 @@
+/* use of if else ... */
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"Enter a number:";
+    cin>>num;
+
+    if(num%2==0)
+    {
+        cout<<"The number is even. ";
+
+    }
+    else
+    {
+        cout<<"The number is odd.";
+    }
+
+
+    getch();
+}
