f08dceab825182b6face9b37e1a5614e8f79c236
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2023-10-16 09:29:50 +0600

    Create c++v21.cpp

---
+++ c++v21.cpp
@@ -0,0 +1,29 @@
+/*uses of if ...*/
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    int num;
+    cout<<"Enter any number:"<<endl;
+    cin>>num;
+
+
+    if(num>0)
+    {
+
+        cout<<"Positive"<<endl;
+    }
+    if(num<0)
+    {
+        cout<<"Negative"<<endl;
+    }
+    if (num==0)
+    {
+
+        cout<<"Zero"<<endl;
+
+    }
+
+    return 0;
+}
