52ee930d7770617bd2c90a4fd56807b0fa5deac6
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2023-10-17 21:03:28 +0600

    Create c++v29.cpp

---
+++ c++v29.cpp
@@ -0,0 +1,27 @@
+//Howo to find a big number using and &&
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1,num2,num3;
+
+
+    cout<<"Enter 3 numbers:"<<endl;
+    cin>>num1>>num2>>num3;
+    if(num1>num2&&num1>>num3)
+    {
+        cout<<num1<<endl;
+    }
+    if(num2>num2&&num2>>num3)
+    {
+        cout<<num2<<endl;
+    }
+    else
+    {
+        cout<<num3<<endl;
+    }
+
+
+    return 0;
+}
