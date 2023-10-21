4546e3369756ad92639749ddbaf3fa94fc3b5ffd
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2023-10-21 11:13:02 +0600

    Create c++v31.cpp

---
+++ c++v31.cpp
@@ -0,0 +1,49 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+
+
+    int mark;
+    cout<<"Enter mark:"<<endl;
+        cin>>mark;
+
+    if(mark>32)
+    {
+        if(mark>=80)
+        {
+            cout<<"A+"<<endl;
+        }
+        else if(mark>=70)
+        {
+            cout<<"A"<<endl;
+        }
+        else if(mark>=60)
+        {
+            cout<<"A-"<<endl;
+        }
+        else if(mark>=50)
+        {
+            cout<<"B"<<endl;
+        }
+        else if(mark>=40)
+        {
+            cout<<"C"<<endl;
+        }
+        else if(mark>=33)
+        {
+            cout<<"D"<<endl;
+        }
+    }
+    else
+    {
+
+
+        cout<<"Fail."<<endl;
+
+    }
+
+
+    return 0;
+}
