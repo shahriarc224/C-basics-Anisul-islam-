0a9c74a079ea95687d8f0f71504e43251c0d647a
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2023-10-22 22:14:27 +0600

    Create c++v33.cpp

---
+++ c++v33.cpp
@@ -0,0 +1,60 @@
+//use of switch statement ....
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int digit;
+    cout<<"Enter a digit:";
+    cin>>digit;
+
+    switch(digit)
+    {
+
+    case 0:
+        cout<<"Zero";
+        break;
+
+    case 1:
+        cout<<"One";
+        break;
+
+    case 2:
+        cout<<"two";
+        break;
+
+    case 3:
+        cout<<"Three";
+        break;
+
+    case 4:
+        cout<<"Four";
+        break;
+
+    case 5:
+        cout<<"Five";
+        break;
+
+    case 6:
+        cout<<"six";
+        break;
+
+    case 7:
+        cout<<"seven";
+        break;
+
+    case 8:
+        cout<<"eight";
+        break;
+
+    case 9:
+        cout<<"nine";
+        break;
+
+
+    }
+
+
+
+    return 0;
+}
