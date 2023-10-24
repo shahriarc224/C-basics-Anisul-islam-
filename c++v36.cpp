5636dc95321bea1867cc945cb51b6ca72fac39ed
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2023-10-24 22:06:37 +0600

    Create c++v36.cpp

---
+++ c++v36.cpp
@@ -0,0 +1,18 @@
+//Use of while loop......
+
+#include<iostream>
+using namespace std;
+int main(){
+
+int i=1;        //initialigion
+while(i<=100)   //condition
+{
+    cout<<i<<endl;
+    i++;        //incriment ,decriment
+}
+
+cout<<"End of while loop."<<endl;
+
+
+
+}
