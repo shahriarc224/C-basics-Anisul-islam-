2805da87ed4fdc1dd787187088e35f67c635d939
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2023-10-26 09:16:40 +0600

    Create c++v37.cpp

---
+++ c++v37.cpp
@@ -0,0 +1,19 @@
+//use of do while loop .....
+
+#include<iostream>
+using namespace std;
+int main(){
+
+int i=1;
+
+do{
+    cout<<i<<endl;
+    i++;
+
+
+}while(i<=100);
+
+cout<<"End of do while loop..."<<endl;
+return 0;
+
+}
