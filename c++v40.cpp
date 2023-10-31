b210f6d36188c88b13cc065b7e24b217267e7035
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2023-10-31 19:14:49 +0600

    Update c++v40.cpp

--- c++v40.cpp
+++ c++v40.cpp
@@ -1,6 +1,24 @@
-/*
+//Multiplication table
 
-ami aita pore uplode korbo 
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+
+    int num;
+    cout<<"Enter any number:"<<endl;
+    cin>>num;
+
+    for(int i=1; i<=10; i++)
+    {
+
+        cout<<num<<"x"<<i<<"="<<num*i<<endl;
+
+    }
 
 
-*/
+   getch();
+
+}
